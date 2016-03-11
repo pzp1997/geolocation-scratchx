f484f76d9d4734a745ce6fdd78a222adc4547a83
Palmer <pzpaul2002@yahoo.com>
2016-03-11 08:41:11 -0500

    v1.0.1-1. fix fatal bug

---
+++ 1.0.1-1/geolocation_extension.js
@@ -0,0 +1,45 @@
+(function(ext) {
+  /* if latLng is true, call callback with latitude else with longitude */
+  var calculateLocation = function(latLng, cb) {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(pos) {
+        cb(latLng ? pos.coords.latitude : pos.coords.longitude);
+      }, function(err) {
+        cb(null);
+      });
+    } else {
+      cb(null);
+    }
+  };
+
+  // Cleanup function when the extension is unloaded
+  ext._shutdown = function() {};
+
+  // Status reporting code
+  // Use this to report missing hardware, plugin or unsupported browser
+  ext._getStatus = function() {
+    return {
+      status: 2,
+      msg: 'Ready'
+    };
+  };
+
+  ext.get_latitude = function(callback) {
+    calculateLocation(true, callback);
+  };
+
+  ext.get_longitude = function(callback) {
+    calculateLocation(false, callback);
+  };
+
+  // Block and block menu descriptions
+  var descriptor = {
+    blocks: [
+      ['R', 'longitude', 'get_longitude'],
+      ['R', 'latitude', 'get_latitude']
+    ]
+  };
+
+  // Register the extension
+  ScratchExtensions.register('Geolocation', descriptor, ext);
+})({});
